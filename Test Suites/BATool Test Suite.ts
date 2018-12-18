c83c9b6e8cacb5e7b3acf158f37f321d64490676
anantadave <aadave15@gmail.com>
2018-12-18 12:01:25 +0530

    test message

--- Test Suites/BATool Test Suite.ts
+++ Test Suites/BATool Test Suite.ts
@@ -4,7 +4,7 @@
    <name>BATool Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-18T11:55:50</lastRun>
+   <lastRun>2018-12-18T12:00:06</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
