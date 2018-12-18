1ad6254c1124fd73a673268b4a95bebd065a65fa
anantadave <aadave15@gmail.com>
2018-12-18 12:10:06 +0530

    test message

--- Test Suites/BATool Test Suite.ts
+++ Test Suites/BATool Test Suite.ts
@@ -4,7 +4,7 @@
    <name>BATool Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-18T12:00:06</lastRun>
+   <lastRun>2018-12-18T12:04:34</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -14,17 +14,17 @@
    <testCaseLink>
       <guid>e435072c-fca2-4e1b-9792-0885aa68812f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Test Case1</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c3f19de8-bfc0-497e-b752-7e40a4fd144c</guid>
+      <guid>99c139c6-2253-43de-bba2-bcc35a2b7f7e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/CreateItemTF</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
-         <id>b9a83d06-3edb-4627-a9a2-d8f918d38531</id>
+         <id>7e2b2c8e-a086-4883-b6c6-d274a0495766</id>
          <iterationEntity>
             <iterationType>ALL</iterationType>
             <value></value>
@@ -32,13 +32,13 @@
          <testDataId>Data Files/Test Data</testDataId>
       </testDataLink>
       <variableLink>
-         <testDataLinkId>b9a83d06-3edb-4627-a9a2-d8f918d38531</testDataLinkId>
+         <testDataLinkId>7e2b2c8e-a086-4883-b6c6-d274a0495766</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Productname</value>
          <variableId>6995d0a2-8281-4b45-ad4b-c3400a2858b2</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>b9a83d06-3edb-4627-a9a2-d8f918d38531</testDataLinkId>
+         <testDataLinkId>7e2b2c8e-a086-4883-b6c6-d274a0495766</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Chaptername</value>
          <variableId>447d2382-6490-4170-ba0f-3f7dd84f72e8</variableId>
